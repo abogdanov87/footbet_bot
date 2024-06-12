cd3d71d3b19652f43fcd0039b657981727e4a60f
BogdanovMac <bogdanov@BogdanovMacs-MacBook-Air.local>
2024-06-12 21:59:45 +0300

    upd

--- main.py
+++ main.py
@@ -1,28 +1,78 @@
 import asyncio
 import logging
+import requests
 import sys
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, Router, F, types
 from aiogram.types.web_app_info import WebAppInfo
-from aiogram.client.default import DefaultBotProperties
+# from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = '7408980611:AAHiXQmEhmrvX8sEfWVEv2ZsUjZPJkGjBIw'
+URL = 'https://footbet.fun/api/v2/tournament-table/1/'
+# URL = 'http://127.0.0.1:8000/api/v2/tournament-table/1/'
+TOKEN = '7408980611:AAHiXQmEhmrvX8sEfWVEv2ZsUjZPJkGjBIw' # PROD
+# TOKEN = '7163748824:AAHRLrUUW308fuQ5JzL39Z7K-SvfqEi_PQk' # TEST
+# bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
+router = Router(name=__name__)
+dp.include_routers(router)
+
+
+def get_table():
+    info_msg = ""
+    
+    response = requests.get(URL)
+    if response.status_code == 200:
+        data = response.json()
+        info_msg = f"""<b>Турнирная таблица {data['title']}</b>
+
+"""
+        position = 1
+        # participants = data['tournament_participant']
+        participants = sorted(
+            data['tournament_participant'], 
+            key=lambda p: (
+                p['score']['points'],
+                p['score']['exact_result'],
+                p['score']['goals_difference'],
+                p['score']['match_result'],
+                (p['user']['nickname'] or p['user']['username'])
+            ), 
+            reverse=True)
+        # import pdb; pdb.set_trace()
+        for participant in participants:
+            username = participant['user']['nickname'] or participant['user']['username']
+            score = participant['score']['points']
+            info_msg += f""" {username} — {score}
+"""
+            position = position + 1
+    return info_msg
+
 
 @dp.message(CommandStart())
 async def handle_start(message: types.Message):
-    btn = types.InlineKeyboardButton(text='Бот', url='https://t.me/footbetFunBot')
-    button_row = [btn]
+    btn1 = types.InlineKeyboardButton(text='Бот', url='https://t.me/footbetFunBot')
+    btn2 = types.InlineKeyboardButton(text='Таблица', callback_data='table')
+    button_row = [btn1, btn2]
     markup = types.InlineKeyboardMarkup(inline_keyboard=[button_row])
+    await message.delete()
     await message.answer('Привет!', reply_markup=markup)
 
+
+@router.callback_query(F.data == 'table')
+async def callback_query_handler(callback_query: types.CallbackQuery):
+    chat_id = callback_query.message.chat.id
+    await callback_query.message.delete()
+    await callback_query.bot.send_message(chat_id, get_table())
+
+
 async def main() -> None:
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
